2338e816fdebb31a694105befcd63239f4f59cfd
ERA <ubuntu@SHADOW-9O8U623A.localdomain>
2021-02-03 14:53:06 +0100

    Bash to shell script conversion

--- getlang.sh
+++ getlang.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-cdn="https://dofusretro.cdn.ankama.com"
-wget -P lang ${cdn}/lang/versions_$1.txt
-lang=$(<lang/versions_$1.txt)
-lang=${lang:3:-1}
-lang=${lang//,/_}
-IFS='|'
-read -ra ADDR <<< "$lang"
-for i in "${ADDR[@]}"; do
-    wget -P lang/swf ${cdn}/lang/swf/$i.swf
+#!/bin/sh
+
+readonly CDN="https://dofusretro.cdn.ankama.com"
+
+wget -qNP lang "${CDN}/lang/versions.swf" "${CDN}/lang/versions_${1}.txt"
+
+tail -c +4 "lang/versions_${1}.txt" | head -c -1 | tr ',' '_' | tr '|' '\n' | while read -r lang; do
+  wget -qNP lang/swf "${CDN}/lang/swf/${lang}.swf"
 done
